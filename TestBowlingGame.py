b54fe41b65d25ae8a88067a3c9b75992077d636f
sosso <xtremerunnerars@gmail.com>
2015-03-04 20:58:42 -0800

    Tests: add rollSpare and rollTimes method to remove unncessary comments

--- TestBowlingGame.py
+++ TestBowlingGame.py
@@ -8,23 +8,27 @@ class TestBowlingGame(unittest.TestCase):
         self.game = BowlingGame()
 
     def testAllGuttersReturns0(self):
-        for _ in range(20):
-            self.game.roll(0)
+        self.rollTimes(0, 20)
         self.assertEquals(0, self.game.score())
 
     def testAll1sReturns20(self):
-        for _ in range(20):
-            self.game.roll(1)
+        self.rollTimes(1, 20)
         self.assertEquals(20, self.game.score())
 
     def testItHandlesSpares(self):
-        self.game.roll(1)
-        self.game.roll(9)
+        self.rollSpare()
         self.game.roll(5)
 
-        for _ in range(17):
-            self.game.roll(0)
+        self.rollTimes(0, 17)
         self.assertEquals(20, self.game.score())
+
+    def rollSpare(self):
+        self.game.roll(1)
+        self.game.roll(9)
+
+    def rollTimes(self, pins_toppled, times):
+        for _ in range(times):
+            self.game.roll(pins_toppled)
 
 if __name__ == '__main__':
     unittest.main()
