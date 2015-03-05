59b827250c45acc75d2bf49d872778956fd6d5b7
Anthony Sosso <asosso@findthebest.com>
2015-03-05 14:38:57 -0800

    Test: test handle strike

--- TestBowlingGame.py
+++ TestBowlingGame.py
@@ -22,9 +22,38 @@ class TestBowlingGame(unittest.TestCase):
         self.rollTimes(0, 17)
         self.assertEquals(20, self.game.score())
 
+    def testItHandlesStrikes(self):
+        self.rollStrike()
+        self.game.roll(5)
+        self.game.roll(3)
+
+        self.rollTimes(0, 17)
+        self.assertEquals(26, self.game.score())
+
+    # def testPerfectGameIs300(self):
+    #     self.rollTimes(10, 12)
+    #     self.assertEquals(300, self.game.score())
+
+    # def testDutch200Is200(self):
+    #     for frame in range(10):
+    #         self.rollStrike()
+    #         self.rollSpare()
+    #     self.assertEquals(200, self.game.score())
+
+    # def testGutterUntilLastFrameTurkeyIs30(self):
+    #     self.rollTimes(0, 18)
+    #     self.rollStrike()
+    #     self.rollStrike()
+    #     self.rollStrike()
+    #
+    #     self.assertEquals(30, self.game.score())
+
     def rollSpare(self):
         self.game.roll(1)
         self.game.roll(9)
+
+    def rollStrike(self):
+        self.game.roll(10)
 
     def rollTimes(self, pins_toppled, times):
         for _ in range(times):
