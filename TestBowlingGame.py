783212af5d7305cc6813fac21f0fd728e9852e35
sosso <xtremerunnerars@gmail.com>
2015-03-04 20:24:23 -0800

    Test: returns 20 for all 1s

--- TestBowlingGame.py
+++ TestBowlingGame.py
@@ -12,5 +12,10 @@ class TestBowlingGame(unittest.TestCase):
             self.game.roll(0)
         self.assertEquals(0, self.game.score())
 
+    def testAll1sReturns20(self):
+        for _ in range(20):
+            self.game.roll(1)
+        self.assertEquals(20, self.game.score())
+
 if __name__ == '__main__':
     unittest.main()
